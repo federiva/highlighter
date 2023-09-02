29fa4a66679c353dbf58e348e7773e4baeb48459
federiva <federivadeneira@gmail.com>
2023-09-01 23:24:29 -0300

    feat: Adding helper functions

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+available_languages <- readLines("./inst/data/languages")
+
+#' @export
+get_available_languages <- function() {
+  available_languages
+}
+
+#' importFrom rlang abort
+#' @noRd
+assert_language_is_available <- function(language) {
+  test <- language %in% available_languages
+  if (!test) {
+    rlang::abort(
+      message = paste(language, "is not a valid language. Check get_available_languages() for the supported ones"),
+      class = "languageNotAvailable"
+    )
+  }
+  invisible()
+}

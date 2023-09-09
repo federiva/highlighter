08dcd8919579bb6cd14a558c26ece5212a67c14a
federiva <federivadeneira@gmail.com>
2023-09-09 01:18:28 -0300

    using man file

---
+++ man/assets/shiny_app_example.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(highlighter)
+
+ui <- fluidPage(
+  selectizeInput(
+    inputId = "file",
+    label = "Select a file",
+    choices = list.files(path.package(package = "highlighter"), recursive = TRUE)
+  ),
+  selectizeInput(
+    inputId = "language",
+    label = "Select language",
+    choices = highlighter::get_available_languages(),
+    selected = "r"
+  ),
+  highlighterOutput("code")
+)
+
+server <- function(input, output, session) {
+  output$code <- renderHighlighter({
+    req(isTruthy(input$language))
+    highlight_file(
+      file_path = file.path(
+        path.package(package = "highlighter"),
+        input$file
+      ),
+      language = input$language,
+      plugins = list(
+        line_number(
+          use_line_number = TRUE,
+          start_from = 3
+        ),
+        highlight(
+          range = "7-10,13"
+        )
+      )
+    )
+  })
+}
+
+shinyApp(ui, server)
